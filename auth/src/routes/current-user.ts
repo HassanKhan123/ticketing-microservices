048619715aae8b0e5ff7058e292efa4daebad2c5
HassanKhan123 <hk2602809@gmail.com>
2022-11-14 14:37:41 +0500

    added: validation

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/api/users/currentuser", (req, res) => {
-  res.send("Hi !");
+  res.send("Hi!!! !");
 });
 
 export { router as currentUserRouter };
